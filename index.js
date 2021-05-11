754b5b94e9499902fe6fcd3f994955fcc828f2ba
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-11 17:36:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,20 +8,3 @@ let totalBatteries=0;
 
 totalBatteries = batteryBatches.reduce((total,x) => x+total, 0)
 
-// const sinon = require( 'sinon' )
-
-// describe('reducer', function() {
-//   describe('batteries', function() {
-//     it('should have a `totalBatteries` variable', function() {
-//       expect(totalBatteries).to.exist;
-//     });
-
-//     it('should have a number as a result', function() {
-//       expect(totalBatteries).to.be.a('number');
-//     });
-
-//     it('should have made the sum of all the assembled batteries', function() {
-//       expect(totalBatteries).to.eql(31);
-//     });
-//   });
-// });
